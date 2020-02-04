66ed4f92c82332962e2a9c86ef2134df148e4b17
boaass <boaass@sina.com>
2020-02-04 18:11:32 +0800

    add 198

---
+++ 198. House Robber/House Robber/main.py
@@ -0,0 +1,41 @@
+# You are a professional robber planning to rob houses along a street. Each house has a certain amount of money
+# stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system
+# connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+#
+# Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount
+# of money you can rob tonight without alerting the police.
+#
+# Example 1:
+#
+# Input: [1,2,3,1]
+# Output: 4
+# Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+#              Total amount you can rob = 1 + 3 = 4.
+# Example 2:
+#
+# Input: [2,7,9,3,1]
+# Output: 12
+# Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+#              Total amount you can rob = 2 + 9 + 1 = 12.
+
+
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        max_nums = [nums[0], max(nums[0], nums[1])]
+        for n in nums[2:]:
+            max_nums[0], max_nums[1] = max_nums[1], max(max_nums[0] + n, max_nums[1])
+        return max(max_nums)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    r = s.rob([2,7,9,3,1])
+    print r
